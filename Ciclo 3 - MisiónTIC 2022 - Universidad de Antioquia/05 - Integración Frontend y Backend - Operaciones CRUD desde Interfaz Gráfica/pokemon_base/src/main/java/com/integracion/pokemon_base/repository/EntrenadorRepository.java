f266e5e513d9938ee07b8f3f8be4ae0560dd1dc2
JuanGIS <ingjuanmasuarez@gmail.com>
2022-09-16 08:07:59 -0500

    Delete Ciclo 3 - MisiónTIC 2022 - Universidad de Antioquia/05 - Integración Frontend y Backend - Operaciones CRUD desde Interfaz Gráfica/pokemon_base directory

--- Ciclo 3 - MisiónTIC 2022 - Universidad de Antioquia/05 - Integración Frontend y Backend - Operaciones CRUD desde Interfaz Gráfica/pokemon_base/src/main/java/com/integracion/pokemon_base/repository/EntrenadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.integracion.pokemon_base.repository;
-
-import com.integracion.pokemon_base.model.Entrenador;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EntrenadorRepository extends JpaRepository<Entrenador, Long> {
-}
--- Ciclo 3 - MisiónTIC 2022 - Universidad de Antioquia/05 - Integración Frontend y Backend - Operaciones CRUD desde Interfaz Gráfica/pokemon_base/src/main/java/com/integracion/pokemon_base/repository/PokemonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.integracion.pokemon_base.repository;
-
-import com.integracion.pokemon_base.model.Pokemon;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
-}
