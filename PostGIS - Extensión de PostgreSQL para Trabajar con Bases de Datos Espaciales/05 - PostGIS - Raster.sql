4c72569cc838c539bbda7dd2312f27ec19aad59d
IngJuanMaSuarez <ingjuanmasuarez@gmail.com>
2023-01-05 10:53:37 -0500

    separar material por temas

--- PostGIS - Extensión de PostgreSQL para Trabajar con Bases de Datos Espaciales/05 - PostGIS - Raster.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-	ST_Height(rast) as num_filas,
-	ST_Width(rast) as num_columnas,
-	ST_SRID(rast) as sistema_referencia,
-	ST_Numbands(rast) as numero_bandas
-FROM chia
-
-SELECT *
-FROM raster_columns
